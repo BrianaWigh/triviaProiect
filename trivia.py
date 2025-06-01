bf249182f6908f760577d3102ea2f084b4f6129e
AndreaVarlan <andreavarlaa55@gmail.com>
2025-06-01 23:00:49 +0300

    Update trivia.py

--- trivia.py
+++ trivia.py
@@ -53,7 +53,7 @@ class TriviaGame:
             self.root.after(1500, self.load_question)
         else:
             self.root.after(1500, self.show_final_score)
-def show_final_score(self):
+    def show_final_score(self):
         for widget in self.root.winfo_children():
             widget.destroy()
         final = f"Scor final: {self.score} din {len(questions)}"
